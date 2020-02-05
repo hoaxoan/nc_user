60e4458365144ef1623c0b8ca927dfca91ebaee8
nguyendo2008 <Nguyendo2008@gmail.com>
2020-02-05 22:41:59 +0700

    nc_user

--- user/delivery/http/handler.go
+++ user/delivery/http/handler.go
@@ -1,21 +1,21 @@
 package http
 
 import (
-	_userUsecase "github.com/hoaxoan/nc_user/user"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/hoaxoan/nc_user/config"
 	"github.com/hoaxoan/nc_user/model"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/hoaxoan/nc_user/user"
 	"github.com/labstack/echo/v4"
-	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
-type UserHandler struct {
-	UUcase _userUsecase.Usecase
+type userHandler struct {
+	UUcase user.Usecase
 }
 
-func NewUserHandler(e *echo.Echo, uc _userUsecase.Usecase) {
-	handler := &UserHandler{
+func NewUserHandler(e *echo.Echo, uc user.Usecase) {
+	handler := &userHandler{
 		UUcase: uc,
 	}
 	PrivateRoute(e, handler)
@@ -23,24 +23,24 @@ func NewUserHandler(e *echo.Echo, uc _userUsecase.Usecase) {
 	PublicRoute(e, handler)
 }
 
-func PrivateRoute(e *echo.Echo, handler *UserHandler) {
+func PrivateRoute(e *echo.Echo, handler *userHandler) {
 	JWTConfig := middleware.JWTConfig{
 		SigningKey: []byte(config.Config.JWTSecret.JWTKey),
 		Claims:     &model.CustomClaims{},
 	}
-	
+
 	g := e.Group("/v1/private")
 	g.Use(middleware.JWTWithConfig(JWTConfig))
 	g.PUT("/user", handler.Update)
 }
 
-func PublicRoute(e *echo.Echo, handler *UserHandler) {
+func PublicRoute(e *echo.Echo, handler *userHandler) {
 	g := e.Group("/v1/public")
 	g.PATCH("/user/login", handler.Auth)
 	g.POST("/user/register", handler.Register)
 }
 
-func (h *UserHandler) Register(ctx echo.Context) error {
+func (h *userHandler) Register(ctx echo.Context) error {
 	var user model.User
 	if err := ctx.Bind(&user); err != nil {
 		return ctx.JSON(http.StatusBadRequest, model.Error{Code: http.StatusBadRequest, Description: "bad request"})
@@ -55,7 +55,7 @@ func (h *UserHandler) Register(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, res.User)
 }
 
-func (h *UserHandler) Update(ctx echo.Context) error {
+func (h *userHandler) Update(ctx echo.Context) error {
 	userToken := ctx.Get("user").(*jwt.Token)
 	if userToken == nil || userToken.Valid == false {
 		return ctx.JSON(http.StatusBadRequest, model.Error{Code: http.StatusBadRequest, Description: "token invalid"})
@@ -84,7 +84,7 @@ func (h *UserHandler) Update(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, res.User)
 }
 
-func (h *UserHandler) Auth(ctx echo.Context) error {
+func (h *userHandler) Auth(ctx echo.Context) error {
 	var user model.User
 	if err := ctx.Bind(&user); err != nil {
 		return ctx.JSON(http.StatusBadRequest, model.Error{Code: http.StatusBadRequest, Description: "bad request"})
